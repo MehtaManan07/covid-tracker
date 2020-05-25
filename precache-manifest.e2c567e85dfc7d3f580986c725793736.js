59d0e9c9460379ea3d7f57ab8f2b1f717d99df5c
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2020-05-25 20:35:16 +0530

    Updates

---
+++ precache-manifest.e2c567e85dfc7d3f580986c725793736.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d37bc79c73e176f59af20c0d6eff21dc",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "f971aafbc60ead6233ea",
+    "url": "/covid-tracker/static/css/main.00a79adf.chunk.css"
+  },
+  {
+    "revision": "a3c14022647fa0ff1039",
+    "url": "/covid-tracker/static/js/2.d6810f6e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.d6810f6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f971aafbc60ead6233ea",
+    "url": "/covid-tracker/static/js/main.e7fee618.chunk.js"
+  },
+  {
+    "revision": "4a2928b2aa18bafd8cab",
+    "url": "/covid-tracker/static/js/runtime-main.387fde33.js"
+  }
+]);
